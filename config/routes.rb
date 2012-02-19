964e1c7d3903aa23f90052227bea4418ccecc1cc
bruno <bruno.eduardo.santos@gmail.com>
2012-02-19 11:09:17 +0000

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   match 'contact', :to => 'pages#contact'
   match 'about', :to => 'pages#about'
   match 'help', :to => 'pages#help'
   root :to => 'pages#home'	
 
+  match '/signup', :to => 'users#new'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -17,5 +17,9 @@ it "should have a Help page at '/help'" do
 get '/help'
 response.should have_selector('title', :content => "Help")
 end
+it "should have a signup page at '/signup'" do
+get '/signup'
+response.should have_selector('title', :content => "Sign up")
+end
 
 end
